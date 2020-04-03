3c98cc6d2576a9a45d65950062c50dd539974f99
unknown <roabigail.diaz@gmail.com>
2020-04-03 00:36:41 -0300

    terminado

--- Guia_ejercicios_16a18/ejercicio18/Program.cs
+++ Guia_ejercicios_16a18/ejercicio18/Program.cs
@@ -12,14 +12,15 @@ namespace PruebaGeometria
         static void Main(string[] args)
         {
             
-            Punto punto1 = new Punto(-1,-3);
-            Punto punto2 = new Punto(2,4);
-            Rectangulo rec = new Rectangulo(punto1, punto2);
+            Punto vertice1 = new Punto(15,4);
+            Punto vertice3 = new Punto(7,12);
+            // vertice2 (-1,4) vertice4(2,-3)
+            Rectangulo rec = new Rectangulo(vertice1, vertice3);
 
             rec.Area();
             rec.Perimetro();
                 
-            Console.WriteLine(Rectangulo.Mostrar(rec));
+            Console.WriteLine(Rectangulo.MostrarDatos(rec));
 
             Console.ReadKey();
         }
--- Guia_ejercicios_16a18/ejercicio18/Rectangulo.cs
+++ Guia_ejercicios_16a18/ejercicio18/Rectangulo.cs
@@ -29,47 +29,72 @@ namespace Geometria
         #endregion
 
         #region Constructor
-        public Rectangulo(Punto vertice1, Punto vertice3)
+        public Rectangulo (Punto vertice1, Punto vertice3)// v1 esta en diagonal al v3
         {
             this.vertice1 = vertice1;
             this.vertice3 = vertice3;
-            this.vertice2 = new Punto(vertice1.GetX(),vertice3.GetY());
+            this.vertice2 = new Punto(vertice1.GetX(), vertice3.GetY());
             this.vertice4 = new Punto(vertice3.GetX(), vertice1.GetY());
-
         }
         #endregion
 
         #region Metodos
+        public float GetArea()
+        {
+            return this.area;
+        }
+
+        public float GetPerimetro()
+        {
+            return this.perimetro;
+        }
+
         public float Area()
         {
             float basee, altura;
+            bool flag = true;
 
-            basee = Math.Abs(this.vertice1.GetX() - this.vertice3.GetX());
-            altura =Math.Abs(this.vertice1.GetY() - this.vertice3.GetY());
-            this.area = basee * altura;
-            return this.area;
+            if(flag)
+            {
+                altura = Math.Abs(this.vertice1.GetX() - this.vertice3.GetX());
+                basee = Math.Abs(this.vertice1.GetY() - this.vertice3.GetY());
+                this.area = basee * altura;
+                flag = false;
+            }
+            return GetArea();
         }
 
         public float Perimetro()
         {
             float basee, altura;
+            bool flag = true;
 
-            basee = Math.Abs(this.vertice1.GetX() - this.vertice3.GetX());
-            altura = Math.Abs(this.vertice1.GetY() - this.vertice3.GetY());
-            this.perimetro = (basee + altura) * 2;
-            return this.perimetro;
+            if(flag)
+            {
+                altura = Math.Abs(this.vertice2.GetX() - this.vertice4.GetX());
+                basee = Math.Abs(this.vertice2.GetY() - this.vertice4.GetY());
+                this.perimetro = (basee + altura) * 2;
+                flag = false;
+            }
+            
+            return GetPerimetro();
         }
 
-
-        public static string Mostrar(Rectangulo datosRec)
+        public static string MostrarDatos(Rectangulo datos)
         {
-            StringBuilder datos = new StringBuilder();
+            StringBuilder cadenaAMostrar = new StringBuilder();
 
-            datos.AppendLine("INFORMACIO DEL RECTANGULO:");
-            datos.AppendLine(string.Format("Area del rectangulo {0}. Perimetro del rectangulo {1}",datosRec.area,datosRec.perimetro));
+            cadenaAMostrar.AppendLine("INFORMACION TRIANGULO: ");
+            cadenaAMostrar.AppendLine(string.Format("\nVertice 1 ({0},{1})", datos.vertice1.GetX(), datos.vertice1.GetY()));
+            cadenaAMostrar.AppendLine(string.Format("Vertice 2 ({0},{1})", datos.vertice2.GetX(), datos.vertice3.GetY()));
+            cadenaAMostrar.AppendLine(string.Format("Vertice 3 ({0},{1})", datos.vertice3.GetX(), datos.vertice3.GetY()));
+            cadenaAMostrar.AppendLine(string.Format("Vertice 4 ({0},{1})", datos.vertice4.GetX(), datos.vertice4.GetY()));
+            cadenaAMostrar.AppendLine(string.Format("\nArea del rectangulo "+datos.GetArea()));
+            cadenaAMostrar.AppendLine(string.Format("Perimetro del rectangulo "+datos.GetPerimetro()));
 
-            return datos.ToString();
+            return cadenaAMostrar.ToString();
         }
+
         #endregion
     }
 }
