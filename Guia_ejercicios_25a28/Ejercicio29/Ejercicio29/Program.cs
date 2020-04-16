db794cc682258a8f2d6563c902b76d2d92b2cb1a
unknown <roabigail.diaz@gmail.com>
2020-04-16 06:35:43 -0300

    terminado

---
+++ Guia_ejercicios_25a28/Ejercicio29/Ejercicio29/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equipo equipo = new Equipo(2,"boca");
+            Jugador j1 = new Jugador(389414, "lucas");
+            Jugador j2 = new Jugador(411,"rocio",5,2);
+            Jugador j3 = new Jugador(411, "refafa", 10, 6);
+
+            if (equipo + j2)
+                Console.WriteLine("ok");
+            else
+                Console.WriteLine("no");
+
+            if (equipo + j3)
+                Console.WriteLine("ok");
+            else
+                Console.WriteLine("no");
+
+            if (equipo + j1)
+                Console.WriteLine("ok");
+            else
+                Console.WriteLine("no");
+
+            Console.WriteLine(equipo.MostrarEquipo(equipo));
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Guia_ejercicios_25a28/Ejercicio29/Entidades/Jugador.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Jugador
+    {
+        private int dni;
+        private int totalGoles;
+        private int partidosJugados;
+        private float promedioGoles;
+        private string nombre;
+
+        private Jugador()
+        {
+            this.partidosJugados = 0;
+            this.totalGoles = 0;
+            this.promedioGoles = 0;
+        }
+
+        public Jugador(int dni, string nombre):this()
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+        }
+        public Jugador(int dni, string nombre, int totalGoles, int totalPartidos)
+            : this(dni, nombre)
+        {
+            this.partidosJugados = totalPartidos;
+            this.totalGoles = totalGoles;
+        }
+
+        public float GetPromedioGoles()
+        {
+            if(this.partidosJugados == 0)
+                this.promedioGoles = 0;
+            else
+               this.promedioGoles = (float)this.totalGoles / this.partidosJugados;
+
+            return this.promedioGoles;
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder cadena = new StringBuilder();
+
+            cadena.AppendLine($"Nombre {this.nombre}");
+            cadena.AppendLine($"DNI {this.dni}");
+            cadena.AppendLine($"Partidos jugandos {this.partidosJugados}");
+            cadena.AppendLine($"Total goles {this.totalGoles}");
+            cadena.AppendLine($"Promedio goles {this.GetPromedioGoles()}");
+
+            return cadena.ToString();
+        }
+
+        public static bool operator==(Jugador j1, Jugador j2)
+        {
+            return (j1.dni == j2.dni);
+        }
+
+        public static bool operator !=(Jugador j1, Jugador j2)
+        {
+            return !(j1.dni == j2.dni);
+        }
+
+    }
+}
