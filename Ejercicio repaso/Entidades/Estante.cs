79ffa1a32297292ea2fd5079ec19e5ee96b42029
unknown <roabigail.diaz@gmail.com>
2020-04-16 03:30:44 -0300

    terminado

---
+++ Ejercicio repaso/Entidades/Estante.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Estante
+    {
+        private int ubicacionEstante;
+        private Producto[] arrayProductos;
+
+        private Estante(int capacidad)
+        {
+            this.arrayProductos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion) :this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.arrayProductos;
+        }
+
+        public static string MostrarEstantes(Estante e)
+        {
+            StringBuilder mostrar = new StringBuilder();
+
+            mostrar.AppendLine($"Ubicacion estante: {e.ubicacionEstante}");
+            mostrar.AppendLine("Productos: ");
+
+            foreach (Producto item in e.arrayProductos)
+            {
+                if (!(item is null))
+                    mostrar.AppendLine(Producto.MostrarProducto(item));
+            }
+
+            return mostrar.ToString();
+        }      
+
+        public static bool operator==(Estante e, Producto p)
+        {
+            return (e.arrayProductos.Contains(p));
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool retorno = false;
+
+            if(e!=p)
+            {
+                for(int i=0; i<e.arrayProductos.Length; i++)
+                {
+                    if(e.arrayProductos[i] is null)
+                    {
+                        e.arrayProductos[i] = p;
+                        return true;
+                    }
+                }
+            }
+
+            return retorno;
+        }
+
+        public static Estante operator-(Estante e, Producto p)
+        {
+            if(e==p)
+            {
+                for (int i = 0; i < e.arrayProductos.Length; i++)
+                {
+                    if (e.arrayProductos[i] == p)
+                    {
+                        e.arrayProductos[i] = null;
+                    }
+                }
+            }
+
+            return e;
+        }
+    }
+}
---
+++ Ejercicio repaso/Entidades/Producto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder mostrar = new StringBuilder();
+
+            mostrar.AppendLine($"Marca {p.GetMarca()}");
+            mostrar.AppendLine($"Precio {p.GetPrecio()}");
+            mostrar.AppendLine($"Codigo de barra {p.codigoDeBarra}");
+
+            return mostrar.ToString();
+        }
+
+        public static explicit operator string(Producto p)
+        {
+           return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            return (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra);
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra);
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            return (p.marca == marca);
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p.marca == marca);
+        }
+    }
+}
