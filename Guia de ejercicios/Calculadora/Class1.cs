72be18daf0403382f26c161e36c7a369985a5d5b
unknown <roabigail.diaz@gmail.com>
2021-03-21 19:08:04 -0300

    ejercicio 15

---
+++ Guia de ejercicios/Calculadora/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Class1
+
+    {
+        static public double Calcular(int num1, int num2, char operador)
+        {
+            double resultado=0;
+
+            switch (operador)
+            {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+
+                case '/':
+                    if (Class1.Validar(num2))
+                        resultado = num1 / num2;
+                    else
+                        Console.WriteLine("no se puede dividir por 0");
+                    break;
+
+                default:
+                    Console.WriteLine("error, operador invalido");
+                    break;
+            }
+
+            return resultado;
+        }
+
+        static private bool Validar(int num2)
+        {
+            bool retorno = false;
+           
+            if (num2 != 0)
+                retorno= true;
+
+            return retorno;
+        }
+
+
+
+    }
+}
---
+++ Guia de ejercicios/Ejercicio15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculadora;
+
+namespace Ejercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            char operacion;
+
+            Console.Write("Ingrese primer numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nIngrese segundo numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nIngrese operacion a realizar (+,-,*,/): ");
+            operacion = Convert.ToChar(Console.ReadLine());
+
+            Console.Clear();
+            Console.Write("\nEl resultado de la operacion es {0}",Class1.Calcular(num1,num2,operacion));
+            Console.ReadKey();
+        }
+    }
+}
