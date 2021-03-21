f375606aa5c8910b17de4095d2480f53fbf376ff
unknown <roabigail.diaz@gmail.com>
2021-03-21 12:54:00 -0300

    ejercicio 13

---
+++ Guia de ejercicios/Conversor/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor
+{
+    public class Class1
+    {
+        static public string DecimalToBinario(int valor)
+        {
+            string binario=string.Empty;
+
+            while(valor>0)
+            {
+                valor = valor / 2;
+                binario = binario + valor % 2;//concateno el resto a la cadena
+            }
+
+            return binario;
+        }
+      
+        static public int BinarioToDecimal(string valor)
+        {
+            char[] arrayEnteros = valor.ToCharArray();
+            Array.Reverse(arrayEnteros);
+            double numDec=0;
+            //los binarios se convierte de derecha a izquiera, invierto cadena
+
+            for(int i=0; i < valor.Length; i++)
+            {
+                if (arrayEnteros[i] == '1')
+                {
+                    numDec += Math.Pow(2, i);
+                }
+            }
+
+            return (int)numDec;
+
+           
+        }
+    }
+}
---
+++ Guia de ejercicios/Ejercicio13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Conversor;
+
+namespace Ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ejercicio numero 13";
+
+            Console.Write("Ingrese numero decimal: ");
+            int dec = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese numero binario: ");
+            string bin = Console.ReadLine();
+
+            Console.Clear();
+            Console.Write("decimal {0} convertido a binario es {1}\n" +
+                          "binario {2} convertido a decimal es {3}",
+                           dec,Class1.DecimalToBinario(dec),
+                           bin,Class1.BinarioToDecimal(bin));
+
+            Console.ReadKey();
+        }
+    }
+}
