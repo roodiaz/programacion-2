43a3d974769505257978bdebad9167fbe4911de2
unknown <roabigail.diaz@gmail.com>
2021-03-21 11:56:55 -0300

    ejercicio 12

--- Guia de ejercicios/Ejercicio12/Program.cs
+++ Guia de ejercicios/Ejercicio12/Program.cs
@@ -10,6 +10,45 @@ namespace Ejercicio12
     {
         static void Main(string[] args)
         {
+            Console.Title = "ejercicio 12";
+
+            char c;
+            int num;
+            int suma=0;
+            string aux;
+
+            do
+            {
+                Console.Write("ingrese numero: ");
+                aux = Console.ReadLine();
+
+                while(!int.TryParse(aux, out num))
+                {
+                    Console.Write("ERROR\ningrese numero: ");
+                    aux = Console.ReadLine();
+                }
+
+                suma += num;
+
+                Console.Write("desea continuar ingresando numeros? s/n: ");
+                c = char.Parse(Console.ReadLine());
+                Console.Clear();
+
+            } while (ValidarS_N(c));
+
+            Console.Write("la suma de los numeros ingresados es: {0}", suma);
+            Console.ReadKey();
+
+        }
+
+        static public bool ValidarS_N(char c)
+        {
+            bool retorno = false;
+
+            if (char.ToLower(c) == 's')
+                retorno= true;
+
+            return retorno;
         }
     }
 }
