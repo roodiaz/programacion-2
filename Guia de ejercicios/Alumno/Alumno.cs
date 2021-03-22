639837b1757be1a5853d264456cabdf0c4110171
unknown <roabigail.diaz@gmail.com>
2021-03-22 19:00:39 -0300

    clase 3

---
+++ Guia de ejercicios/Alumno/Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio16
+{
+    public class Alumno
+    {
+        //variables
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+        string apellido;
+        string nombre;
+        int legajo;
+    
+        public static void CalcularFinal()
+        {
+
+        }
+
+        public static void Estudiar(byte notaUno, byte notaDos)
+        {
+
+        }
+        
+        public static string Mostrar()
+        {
+            return "";
+        }
+
+    }
+}
--- Guia de ejercicios/Ejercicio16/Program.cs
+++ Guia de ejercicios/Ejercicio16/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ejercicio16;
 
 namespace Ejercicio16
 {
@@ -10,6 +11,16 @@ namespace Ejercicio16
     {
         static void Main(string[] args)
         {
+            Console.Title = "ejercicio 16";
+
+            Alumno alumno1 = new Alumno();
+            Alumno alumno2 = new Alumno();
+            Alumno alumno3 = new Alumno();
+
+            
+
+
+            
         }
     }
 }
