fc2c456deaa0a5bc9a682b84753713bb6ee7760e
unknown <roabigail.diaz@gmail.com>
2021-04-05 21:31:02 -0300

    clase 7

---
+++ Ejercicio Integrador(Clase 5)/Clases/Estantes.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Estante
+    {
+
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+
+        /*El constructor de instancia privado será el único que inicializará el array. 
+          La sobrecarga pública del constructor inicializará la
+          ubicación del estante, recibiendo como parámetro capacidad y ubicación.
+          Reutilizar código.*/
+        private Estante(int capacidad)
+        {
+            productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion)
+            : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        /*El método público de clase MostrarEstante, retornará una cadena con toda 
+          la información del estante, incluyendo el
+          detalle de cada uno de sus productos. Reutilizar código. */
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Estante: {e.ubicacionEstante}");
+
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if (!Object.ReferenceEquals(e.productos[i], null))//sino esta vacio
+                    sb.AppendLine(Producto.MostrarProductos(e.productos[i]));
+            }
+            return sb.ToString();
+        }
+
+        /*
+        Sobrecarga de operadores:
+        Igualdad, retornará true, si es que el producto ya se encuentra en el estante, false, caso contrario.
+        */
+        public static bool operator ==(Estante e, Producto p)
+        {
+
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if (e.productos[i] == p)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        /*Adición, retornará true y agregará el producto si el estante posee capacidad de almacenar al menos un producto más y
+        dicho producto no se encuentra en él; false, caso contrario. Reutilizar código.
+        */
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            if (e != p)//retulizo sobrecarga
+            {
+                for (int i = 0; i < e.productos.Length; i++)//recorro lista de productos
+                {
+                    if(e.productos[i] is null)//si hay espacio en estante agrego producto
+                    {
+                        e.productos[i] = p;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+
+        }
+
+        /*Sustracción (Estante, Producto), retornará un estante sin el producto, siempre y cuando el producto se encuentre en el
+        listado. Reutilizar código.*/
+        public static Estante operator -(Estante e, Producto p)
+        {
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if (e == p)//si el producto se encuentra en el estante
+                {
+                    e.productos[i] = null;
+                    break;
+                }
+            }
+
+            return e;
+        }
+
+    }
+}
---
+++ Ejercicio Integrador(Clase 5)/Clases/Producto.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.codigoDeBarra = codigo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProductos(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Marca: {p.GetMarca()}");
+            sb.AppendLine($"Precio: {p.GetPrecio()}");
+            sb.AppendLine($"Codigo de barra: {p.codigoDeBarra}");
+
+            return sb.ToString();
+       
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        //Igualdad (Producto, Producto). Retornará true, si las marcas 
+        //y códigos de barras son iguales, false, caso contrario.
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool retorno = false;
+
+            if(!(p1 is null || p2 is null))//sino estan vacios
+            {
+                if (p1.codigoDeBarra == p2.codigoDeBarra && p1.marca == p2.marca)
+                    retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        //gualdad (Producto, string). Retornará true, si la marca del producto coincide 
+        //con la cadena pasada por parámetro, false,caso contrario.
+        public static bool operator ==(Producto p, string marca)
+        {
+            return p.marca == marca; //si son iguales retorna true
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+    }
+}
