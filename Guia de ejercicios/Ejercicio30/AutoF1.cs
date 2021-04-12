d310fbd33f9d6deb769f27c8a7b84eb47ddae302
Rocio Diaz <roabigail.diaz@gmail.com>
2021-04-11 23:38:36 -0300

    beep

---
+++ Guia de ejercicios/Ejercicio30/AutoF1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio30
+{
+    public class AutoF1
+    {
+        private short cantidadCumbustible;
+        private short vueltasRestantes;
+        private short numero;
+        private string escuderia;
+        private bool enCompetencia;
+
+        #region constructores
+        public AutoF1()
+        {
+            this.enCompetencia = false;
+            this.cantidadCumbustible = 0;
+            this.vueltasRestantes = 0;
+        }
+
+        public AutoF1(short numero, string escuderia) 
+            : this()
+        {
+            this.numero = numero;
+            this.escuderia = escuderia;
+        }
+        #endregion
+
+        #region getters/setters
+        public short GetCombustible() { return this.cantidadCumbustible; }
+        public void SetCombustible(short combustible) { this.cantidadCumbustible = combustible; }
+
+        public short GetVueltas() { return this.vueltasRestantes; }
+        public void SetVueltas(short vueltas) { this.vueltasRestantes = vueltas; }
+        
+        public bool GetCompetencia() { return this.enCompetencia; }
+        public void SetCompetencia(bool competencia) { this.enCompetencia = competencia; }
+
+        #endregion
+
+        #region operadores
+        //b. Dos autos serán iguales si el número y escudería son iguales.
+        public static bool operator ==(AutoF1 a1, AutoF1 a2)
+        {
+            return (a1.numero == a2.numero && a1.escuderia == a2.escuderia);
+        }
+        public static bool operator !=(AutoF1 a1, AutoF1 a2)
+        {
+            return !(a1.numero == a2.numero && a1.escuderia == a2.escuderia);
+        }
+        #endregion
+
+        public string MostrarAuto()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Escuderia {this.escuderia}");
+            sb.AppendLine($"Numero {this.numero}");
+            sb.AppendLine($"En competencia {GetCompetencia()}");
+            sb.AppendLine($"Vueltas restantes {GetVueltas()}");
+            sb.AppendLine($"Combustible restante {GetCombustible()}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Guia de ejercicios/Ejercicio30/Competencia.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio30
+{
+    public class Competencia
+    {
+        private short cantidadCompetidores;
+        private short cantidadVueltas;
+        private List<AutoF1> competidores;
+
+        private Competencia()
+        {
+            this.competidores = new List<AutoF1>();
+        }
+
+        public Competencia(short cantidadVueltas, short cantidadCompetidores) : this()
+        {
+            this.cantidadCompetidores = cantidadCompetidores;
+            this.cantidadVueltas = cantidadVueltas;
+        }
+
+        public string MostrarCompetencia(Competencia c)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Cantidad vueltas {this.cantidadVueltas}");
+            sb.AppendLine($"Cantidad competidores {this.cantidadCompetidores}");
+            sb.AppendLine($"Competiodes: ");
+
+            foreach (AutoF1 item in c.competidores)
+            {
+                sb.AppendLine(item.MostrarAuto());
+            }
+
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Competencia c, AutoF1 a)
+        {
+            return c.competidores.Contains(a);
+        }
+        public static bool operator !=(Competencia c, AutoF1 a)
+        {
+            return !(c.competidores.Contains(a));
+        }
+
+        public static bool operator +(Competencia c, AutoF1 a)
+        {
+            bool todoOk = false;
+            Random rnd = new Random();
+
+            if (c.competidores.Count < c.cantidadCompetidores)
+            {
+                if (c != a)
+                {
+                    a.SetCompetencia(true);
+                    a.SetVueltas(c.cantidadVueltas);
+                    a.SetCombustible((short)rnd.Next(15, 100));
+                    todoOk = true;
+                }
+
+            }
+
+            return todoOk;
+        }
+    }
+}
