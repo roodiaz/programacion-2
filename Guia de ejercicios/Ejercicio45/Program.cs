6cdf926e9e1167ad540ed0a18ab990c78822eba1
Rocio Diaz <roabigail.diaz@gmail.com>
2021-05-16 16:58:13 -0300

    beep

---
+++ Guia de ejercicios/Ejercicio45/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                OtraClase aux = new OtraClase();
+                aux.CapturarExMiClase();
+            }
+            catch (MiExcepcion e)
+            {
+                //e.	MiException será capturada en el Main,
+                // mostrando el mensaje de error que esta almacena por pantalla
+                // y los mensajes de todas las excepciones almacenadas en sus innerException.
+                Console.WriteLine(e.Message);
+                Exception ie = e.InnerException; //guardo la IE
+
+                while (!Object.ReferenceEquals(ie, null))
+                {
+                    Console.WriteLine(ie.Message);
+                    ie = ie.InnerException;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Guia de ejercicios/Ejercicio45/TestUnitarios/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejercicio42;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Metodo_Estatico_LanzarDivideByZero_MiClase()
+        {
+            MiClase.LanzarDivideByZero();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Constructor_SinParam_MiClase()
+        {
+            MiClase clase = new MiClase();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UnaExcepcion))]
+        public void Constructor_ConParam_MiClase()
+        {
+            MiClase clase = new MiClase(String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MiExcepcion))]
+        public void Metodo_Instancia_OtraClase()
+        {
+            OtraClase aux = new OtraClase();
+
+            aux.CapturarExMiClase();
+        }
+    }
+}
