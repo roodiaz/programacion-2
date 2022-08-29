5fc122cba991684199402e93bfb59314e8a78281
Rocio Diaz <47401715+roodiaz@users.noreply.github.com>
2022-08-29 20:57:37 -0300

    Update view

--- Guia de ejercicios/Ejercicio17/Program.cs
+++ Guia de ejercicios/Ejercicio17/Program.cs
@@ -9,75 +9,112 @@ namespace Ejercicio17
 {
     class Program
     {
-        static void Main(string[] args)
+                static void Main(string[] args)
         {
-            Boligrafo bluePen = new Boligrafo(0, ConsoleColor.Blue);
-            Boligrafo redPen = new Boligrafo(5, ConsoleColor.Red);
+            Boligrafo bluePen = new Boligrafo(100, ConsoleColor.Blue);
+            Boligrafo redPen = new Boligrafo(50, ConsoleColor.Red);
             string opcion;
             string dibujo;
+            string recarga;
 
             do
             {
-                Console.Write("1. Azul\n2. Rojo\n\nOpcion: ");
+                Console.Clear();
+
+                Console.Write(
+                    "1. Azul\n" +
+                    "2. Rojo\n" +
+                    "3. Salir\n\n" +
+                    "Opcion: "
+                    );
+
                 opcion = Console.ReadLine();
 
-            } while (!(opcion != "1" || opcion != "2"));
+                Console.Clear();
 
-            Console.Clear();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Cantidad de tinta disponible: " + bluePen.GetTinta());
+                        Console.Write("\nIngrese cantidad de tinta a gastar: ");
+                        short gasto = Convert.ToByte(Console.ReadLine());
 
-            switch (opcion)
-            {
-                case "1":
-                    Console.WriteLine("Cantidad de tinta disponible: " + bluePen.GetTinta());
-                    Console.Write("\nIngrese cantidad de tinta a gastar: ");
-                    short gasto = Convert.ToByte(Console.ReadLine());
+                        if (bluePen.Pintar(gasto, out dibujo))
+                        {
+                            Console.Write("\nTinta gastada:");
+                            Console.ForegroundColor = bluePen.GetColor();
+                            Console.WriteLine(dibujo);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("Cantidad de tinta disponible: " + bluePen.GetTinta());
+                        }
+                        else
+                        {
+                            Console.Write("\nNo hay suficiente tinta, aplicar recarga de tinta? S/N: ");
+                            recarga = Console.ReadLine().ToUpper();
+                            
+                            while(recarga != "S" && recarga != "N")
+                            {
+                                Console.Write("\nAplicar recarga de tinta? S/N: ");
+                                recarga = Console.ReadLine();
+                            }
 
-                    if (bluePen.Pintar(gasto, out dibujo))
-                    {
-                        Console.Write("\nDibujo:");
-                        Console.ForegroundColor = bluePen.GetColor();
-                        Console.WriteLine(dibujo);
-                        
-                    }
-                    else
-                        Console.Write("\nNo hay suficiente tinta");
+                            if(recarga == "S")
+                            {
+                                Console.WriteLine("\n\nAplicando recarga de tinta...");
+                                bluePen.Recargar();
+                                Console.WriteLine("\nCantidad de tinta actual: " + bluePen.GetTinta());
+                            }
+                            else
+                            {
+                                break;
+                            }                          
+                        }
+                        break;
 
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("\n\nAplicando recarga de tinta...");
-                    bluePen.Recargar();
-                    Console.WriteLine("\nCantidad de tinta actual: " + bluePen.GetTinta());
-
-                    break;
-
-                case "2":
-                    Console.WriteLine("Cantidad de tinta disponible: " + redPen.GetTinta());
-                    Console.Write("\nIngrese cantidad de tinta a gastar: ");
-                    short gastoRed = Convert.ToByte(Console.ReadLine());
-
-                    if (redPen.Pintar(gastoRed, out dibujo))
-                    {
-                        Console.Write("\nTinta gastada:");
-                        Console.ForegroundColor = redPen.GetColor();
-                        Console.WriteLine(dibujo);
+                    case "2":
                         Console.WriteLine("Cantidad de tinta disponible: " + redPen.GetTinta());
+                        Console.Write("\nIngrese cantidad de tinta a gastar: ");
+                        short gastoRed = Convert.ToByte(Console.ReadLine());
 
-                    }
-                    else
-                    {
-                        Console.Write("\nTinta gastada:");
-                        Console.ForegroundColor = redPen.GetColor();
-                        Console.WriteLine(dibujo);
-                        Console.Write("\nNo hay suficiente tinta");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine("\n\nAplicando recarga de tinta...");
-                        redPen.Recargar();
-                        Console.WriteLine("\nCantidad de tinta actual: " + redPen.GetTinta());
-                    }
-                        
-                    break;
-            }
+                        if (redPen.Pintar(gastoRed, out dibujo))
+                        {
+                            Console.Write("\nTinta gastada:");
+                            Console.ForegroundColor = redPen.GetColor();
+                            Console.WriteLine(dibujo);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("Cantidad de tinta disponible: " + redPen.GetTinta());
+                        }
+                        else
+                        {
+                            Console.Write("\nNo hay suficiente tinta, aplicar recarga de tinta? S/N: ");
+                            recarga = Console.ReadLine().ToUpper();
 
-            Console.ReadKey();
+                            while (recarga != "S" && recarga != "N")
+                            {
+                                Console.Write("\nAplicar recarga de tinta? S/N: ");
+                                recarga = Console.ReadLine();
+                            }
+
+                            if (recarga == "S")
+                            {
+                                Console.WriteLine("\n\nAplicando recarga de tinta...");
+                                redPen.Recargar();
+                                Console.WriteLine("\nCantidad de tinta actual: " + redPen.GetTinta());
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        break;
+                }
+
+                Console.ReadKey();
+
+            } while (opcion != "3");
+
         }
+
     }
 }
